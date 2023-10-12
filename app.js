b5989618b7c2e91bfe5004ff619314200f1f072e
Mir Billadin <mir.billadin4@gmail.com>
2023-10-12 21:07:49 +0600

    basic API route setup

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
+const tasks = require('./routes/tasks');
 
+
+//Middleware
+app.use(express.json());
+
+
+//Routes
+app.use('/api/v1/tasks', tasks);
+
+
+//Port
 const port = 3000;
-
 app.listen(port, console.log(`Server is listening to ${port}...`));
---
+++ controllers/tasks.js
@@ -0,0 +1,23 @@
+const getAllTasks = (req, res) => {
+    res.send('All tasks from controller');
+}
+const getTask = (req, res) => {
+    res.json(req.params.id);
+}
+const createTask = (req, res) => {
+    res.json(req.body);
+}
+const updateTask = (req, res) => {
+    res.send('Update task');
+}
+const deleteTask = (req, res) => {
+    res.send('Delete task');
+}
+
+module.exports = {
+    getAllTasks,
+    getTask,
+    createTask,
+    updateTask,
+    deleteTask
+}
