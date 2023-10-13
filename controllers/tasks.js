4feaec817f1d108cb04a13b0e3c765157342b681
Mir Billadin <mir.billadin4@gmail.com>
2023-10-14 00:06:29 +0600

    updateTask api added

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -32,9 +32,24 @@ const createTask = async (req, res) => {
 }
 
 
-const updateTask = (req, res) => {
-    res.send('Update task');
+const updateTask = async (req, res) => {
+    try {
+        const {id:taskID} = req.params;
+        const task = await Task.findOneAndUpdate({_id:taskID}, req.body, {
+            new: true,
+            runValidators: true,
+        });
+        if (!task) {
+            return res.status(404).json({message: 'No task found'})
+        }
+        res.status(200).json({task});
+    } catch (error) {
+        res.status(500).json({message: error})
+    }
 }
+
+
+
 const deleteTask = async (req, res) => {
     try {
         const {id:taskID} = req.params;
