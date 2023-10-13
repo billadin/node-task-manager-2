d3882e7e433ce0a37a666dd6ba5bb598253287ba
Mir Billadin <mir.billadin4@gmail.com>
2023-10-13 23:38:11 +0600

    getAllTask, createTask api added

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,15 +1,27 @@
 const Task = require('../models/Task');
 
-const getAllTasks = (req, res) => {
-    res.send('All tasks from controller');
+const getAllTasks = async (req, res) => {
+    try {
+        const task = await Task.find({})
+        res.status(200).json({task})
+    } catch (error) {
+        res.status(500).json({message: error})
+    }
 }
 const getTask = (req, res) => {
     res.json(req.params.id);
 }
+
 const createTask = async (req, res) => {
-    const task = await Task.create(req.body)
-    res.status(201).json({task});
+    try {
+        const task = await Task.create(req.body)
+        res.status(201).json({task});
+    } catch (error) {
+        res.status(500).json({message: error})
+    }
 }
+
+
 const updateTask = (req, res) => {
     res.send('Update task');
 }
