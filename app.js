e9739d2c3103d34a02a9d28e0d10ddfa485434e7
Mir Billadin <mir.billadin4@gmail.com>
2023-10-13 23:11:06 +0600

    db connection setup

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks');
-
-
+const users = require('./app')
+const client = require('./db/connect');
+const connectDB = require('./db/connect');
+require('dotenv').config()
 //Middleware
 app.use(express.json());
-
 
 //Routes
 app.use('/api/v1/tasks', tasks);
@@ -13,4 +14,13 @@ app.use('/api/v1/tasks', tasks);
 
 //Port
 const port = 3000;
-app.listen(port, console.log(`Server is listening to ${port}...`));
+const start = async () => {
+try {
+    await connectDB(process.env.MONGO_URI)
+    app.listen(port, console.log(`Server is listening to ${port}...`));
+} catch (error) {
+    console.log(error);
+}
+}
+
+start();
--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,11 +1,14 @@
+const Task = require('../models/Task');
+
 const getAllTasks = (req, res) => {
     res.send('All tasks from controller');
 }
 const getTask = (req, res) => {
     res.json(req.params.id);
 }
-const createTask = (req, res) => {
-    res.json(req.body);
+const createTask = async (req, res) => {
+    const task = await Task.create(req.body)
+    res.status(201).json({task});
 }
 const updateTask = (req, res) => {
     res.send('Update task');
@@ -14,10 +17,11 @@ const deleteTask = (req, res) => {
     res.send('Delete task');
 }
 
+
 module.exports = {
     getAllTasks,
     getTask,
     createTask,
     updateTask,
-    deleteTask
+    deleteTask,
 }
